5cdebc815c2b32b918e993102976a2e8d0dc1395
chikiryau1 <topmastertop97@gmail.com>
2021-01-16 20:01:30 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import fastify, { FastifyInstance } from "fastify";
+import { Server, ServerResponse, IncomingMessage } from "http";
+
+const server: FastifyInstance<
+  Server,
+  IncomingMessage,
+  ServerResponse
+> = fastify({
+  logger: true,
+});
+
+const start: () => void = async () => {
+  try {
+    await server.listen(3000, "0.0.0.0");
+  } catch (e) {
+    server.log.error(e);
+    process.exit(1);
+  }
+};
+
+start();
