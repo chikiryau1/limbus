1edd2acc247e2fd789514789944e85e4a57460b7
chikiryau1 <topmastertop97@gmail.com>
2021-01-18 22:18:12 +0300

    ping-pong

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,21 +1,28 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import './App.css';
 
 const App: FC = () => {
+  const [value, setValue] = useState<{ response?: string }>({});
+  useEffect(() => {
+    async function request(path: string): Promise<Record<string, unknown>> {
+      const res = await fetch(path);
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const data: Record<string, unknown> = await res.json();
+      setValue(data);
+
+      return data
+    }
+
+    request('http://localhost:3001/ping').then(console.log).catch(console.log);
+  }, []);
   return (
     <div className='App'>
       <header className='App-header'>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
+        {value?.response}
       </header>
     </div>
   );
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,5 +1,6 @@
 import fastify, { FastifyInstance } from 'fastify';
 import { Server, ServerResponse, IncomingMessage } from 'http';
+import cors from 'fastify-cors';
 
 const server: FastifyInstance<
   Server,
@@ -9,8 +10,14 @@ const server: FastifyInstance<
   logger: true,
 });
 
+server.get('/ping', async (request, reply) => {
+  return { response: 'pong' }
+});
+
 const start: () => void = async () => {
   try {
+    // @ts-ignore
+    await server.register(cors);
     await server.listen(3001, '0.0.0.0');
   } catch (e) {
     server.log.error(e);
